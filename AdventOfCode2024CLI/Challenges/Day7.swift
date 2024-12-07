d4d388857086471315d1e61ee5711b6cebe7f40d
Simon Salomons <simon@nextapps.be>
2024-12-07 07:59:16 +0100

    Day 7

--- AdventOfCode2024CLI/Challenges/Day7.swift
+++ AdventOfCode2024CLI/Challenges/Day7.swift
@@ -8,5 +8,64 @@
 public func day7() {
     let content = content(file: "input7")
 
-    
+    struct Equation {
+        let testValue: Int
+        let operands: [Int]
+    }
+
+    let equations = content
+        .split(separator: "\n", omittingEmptySubsequences: true)
+        .map { string in
+            let components = string.split(separator: ":")
+            return Equation(
+                testValue: Int(components[0])!,
+                operands: components[1]
+                    .split(separator: " ", omittingEmptySubsequences: true)
+                    .map({ Int($0)! })
+            )
+        }
+
+    func solve(operands: [Int], operations: [(Int, Int) -> Int]) -> Set<Int> {
+        var operands = operands
+        let lhs = operands[0]
+        let rhs = operands[1]
+        operands.removeFirst(2)
+
+        var outputs: Set<Int> = []
+        for o in operations {
+            let output = o(lhs, rhs)
+            if operands.isEmpty {
+                outputs.insert(output)
+            } else {
+                var operands = operands
+                operands.insert(output, at: 0)
+                outputs.formUnion(solve(operands: operands, operations: operations))
+            }
+        }
+        return outputs
+    }
+
+    func calibrationResult(for operations: [(Int, Int) -> Int]) -> Int {
+        var calibrationResult = 0
+        for equation in equations {
+            let outputs = solve(operands: equation.operands, operations: operations)
+
+            if outputs.contains(equation.testValue) {
+                calibrationResult += equation.testValue
+            }
+        }
+        return calibrationResult
+    }
+
+    let part1 = calibrationResult(for: [
+        { $0 + $1 },
+        { $0 * $1 }
+    ])
+    print(part1)
+    let part2 = calibrationResult(for: [
+        { $0 + $1 },
+        { $0 * $1 },
+        { Int("\($0)\($1)")! }
+    ])
+    print(part2)
 }
