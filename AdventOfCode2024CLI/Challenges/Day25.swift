d085a65ab61fa82f8f2a3ccb41d4c2ac990ff56d
Simon Salomons <simon@nextapps.be>
2024-12-25 07:50:27 +0100

    Day 25

--- AdventOfCode2024CLI/Challenges/Day25.swift
+++ AdventOfCode2024CLI/Challenges/Day25.swift
@@ -6,9 +6,34 @@
 //
 
 func day25() {
-    var content = content(file: "input25")
+    let content = content(file: "input25")
 
-    content = """
+    var locks: [[Int]] = []
+    var keys: [[Int]] = []
+    content.split(separator: "\n\n").forEach { string in
+        let grid = string.grid()
+        if string.first == "#" {
+            locks.append((0..<5).map({ x in
+                (0..<7).first(where: { grid[$0][x] == "." })! - 1
+            }))
+        } else {
+            keys.append((0..<5).map({ x in
+                6 - (0..<7).first(where: { grid[$0][x] == "#" })!
+            }))
+        }
+    }
 
-"""
+    var sum = 0
+    for lock in locks {
+        keyloop: for key in keys {
+            for (l, k) in zip(lock, key) {
+                if l + k > 5 {
+                    continue keyloop
+                }
+            }
+            sum += 1
+        }
+    }
+
+    print(sum)
 }
